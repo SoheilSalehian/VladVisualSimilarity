a73d82f8c207fc81f58d69c88863ef043a831d0b
SoheilSalehian <soheil.salehian@gmail.com>
2017-06-22 15:39:19 -0500

    Simplify Dokerfile and add kmeans bit

--- vlad.py
+++ vlad.py
@@ -2,13 +2,21 @@ import cv2
 import sys
 import numpy as np
 import glob
+import itertools
+from sklearn.cluster import KMeans
 
-def extractVladDescriptor(image):
-  pass
+def extractVladDescriptor():
+  desc = extractSIFT(sys.argv[1])
+
+  # FIXME: the cluster is either 64 or 256 for decent results
+  print kMeans(desc, 8)
+
+
 
 def extractSIFT(imagePath):
   descriptors = list()
 
+  # FIXME: handle all kinds of image types
   for path in glob.glob(imagePath+"/*.jpg"):
     print "Extracting from"+path
     # SIFT Feature Extraction
@@ -19,7 +27,11 @@ def extractSIFT(imagePath):
 
     descriptors.append(siftDescriptors)
 
+  descriptors = list(itertools.chain.from_iterable(descriptors))
+  descriptors = np.asarray(descriptors)
+
   return descriptors
+
 
 # TODO: Additional features for future use
 def extractSURF(image):
@@ -28,5 +40,8 @@ def extractSURF(image):
 def extractORB(image):
   pass
 
-print extractSIFT(sys.argv[1])
+def kMeans(trainingDescriptors, k):
+  est = KMeans(n_clusters=k,init='k-means++',tol=0.0001,verbose=1).fit(trainingDescriptors)
+  return est
 
+extractVladDescriptor()
