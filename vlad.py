df5a0ed396d582a800e9b60c97c28da10b28bf6c
SoheilSalehian <soheil.salehian@gmail.com>
2017-06-22 15:08:42 -0500

    Add SIFT extraction for a directory of images

--- vlad.py
+++ vlad.py
@@ -1,24 +1,32 @@
 import cv2
 import sys
 import numpy as np
+import glob
 
 def extractVladDescriptor(image):
   pass
 
-def extractSURF(imagePath):
-  # SURF Feature Extraction
-  image = cv2.imread(imagePath)
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  sift = cv2.xfeatures2d.SIFT_create()
-  (kps, descriptors) = sift.detectAndCompute(gray, None)
-  return kps, descriptors
+def extractSIFT(imagePath):
+  descriptors = list()
 
-# TODO: Additional features for future
-def extractSIFT(image):
+  for path in glob.glob(imagePath+"/*.jpg"):
+    print "Extracting from"+path
+    # SIFT Feature Extraction
+    image = cv2.imread(path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    (kps, siftDescriptors) = sift.detectAndCompute(gray, None)
+
+    descriptors.append(siftDescriptors)
+
+  return descriptors
+
+# TODO: Additional features for future use
+def extractSURF(image):
   pass
 
 def extractORB(image):
   pass
 
-print extractSURF(sys.argv[1])
+print extractSIFT(sys.argv[1])
 
