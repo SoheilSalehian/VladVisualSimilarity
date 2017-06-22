c6d1f6b709ab96f165e9ec2cb1703e7b2b40f80b
SoheilSalehian <soheil.salehian@gmail.com>
2017-06-22 14:37:59 -0500

    Add basic skeleton of sift

---
+++ vlad.py
@@ -0,0 +1,24 @@
+import cv2
+import sys
+import numpy as np
+
+def extractVladDescriptor(image):
+  pass
+
+def extractSURF(imagePath):
+  # SURF Feature Extraction
+  image = cv2.imread(imagePath)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  surf = cv2.xfeatures2d.SIFT_create()
+  (kps, descriptors) = sift.detectAndCompute(gray, None)
+  return kp, descriptors
+
+# TODO: Additional features for future
+def extractSIFT(image):
+  pass
+
+def extractORB(image):
+  pass
+
+print extractSURF(sys.argv[1])
+
